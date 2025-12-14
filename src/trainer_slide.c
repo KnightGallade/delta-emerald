1b5cf8ed3c3704d9b81f6fffadd5bb8ee3aa208c
KnightGallade <pokey.pineview@gmail.com>
2025-12-14 10:43:58 +0100

    Remove leftover PWT Slide testing

--- src/trainer_slide.c
+++ src/trainer_slide.c
@@ -66,13 +66,6 @@ static const u8* const sFrontierTrainerSlides[DIFFICULTY_COUNT][FRONTIER_TRAINER
     },
 };
 
-static const u8* const sPWTTrainerSlides[DIFFICULTY_COUNT][FRONTIER_TRAINERS_COUNT][TRAINER_SLIDE_COUNT] =
-{
-    [DIFFICULTY_NORMAL] =
-    {
-    },
-};
-
 static const u8* const sTestTrainerSlides[DIFFICULTY_COUNT][TRAINERS_COUNT][TRAINER_SLIDE_COUNT] =
 {
 #include "../test/battle/trainer_slides.h"
@@ -118,8 +111,6 @@ static const u8* const *GetTrainerSlideArray(enum DifficultyLevel difficulty, u3
 {
     if (gBattleTypeFlags & BATTLE_TYPE_FRONTIER)
         return sFrontierTrainerSlides[difficulty][trainerId];
-    else if (gBattleTypeFlags & BATTLE_TYPE_PWT)
-        return sPWTTrainerSlides[difficulty][trainerId];
     else if (TESTING)
         return sTestTrainerSlides[difficulty][trainerId];
     else
